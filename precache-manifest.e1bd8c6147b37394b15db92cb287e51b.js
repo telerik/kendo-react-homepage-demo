61fb74142f9fdaaf455b6b2ef85560088185dde6
Kiril Peyanski <peyanski@progress.com>
2019-03-11 11:31:59 +0200

    Updates

--- precache-manifest.e1bd8c6147b37394b15db92cb287e51b.js
+++ precache-manifest.e1bd8c6147b37394b15db92cb287e51b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/kendo-react-homepage-demo/static/js/runtime~main.963b9a10.js"
   },
   {
-    "revision": "7d2f04f60390338dc65b",
-    "url": "/kendo-react-homepage-demo/static/js/main.7d2f04f6.chunk.js"
+    "revision": "b191d4e3c247c1a4ceb2",
+    "url": "/kendo-react-homepage-demo/static/js/main.b191d4e3.chunk.js"
   },
   {
     "revision": "ef3067d3b8e10c277d02",
     "url": "/kendo-react-homepage-demo/static/js/1.ef3067d3.chunk.js"
   },
   {
-    "revision": "7d2f04f60390338dc65b",
-    "url": "/kendo-react-homepage-demo/static/css/main.5b29709d.chunk.css"
+    "revision": "b191d4e3c247c1a4ceb2",
+    "url": "/kendo-react-homepage-demo/static/css/main.64adb41b.chunk.css"
   },
   {
-    "revision": "66a3b8f2f0d8a1093fee579e4787d178",
+    "revision": "c0afb5445aa25ffd8a0bc672b6c39283",
     "url": "/kendo-react-homepage-demo/index.html"
   }
 ];
