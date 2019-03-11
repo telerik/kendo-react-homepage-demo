c237ac4217d48147cab7c922e982f4b3dfeab051
Kiril Peyanski <peyanski@progress.com>
2019-03-11 10:18:39 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# kendo-react-issues-grids
---
+++ precache-manifest.d478a6d6ecf83221c51c441b9a559d90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fff4aad525e30a55075c",
+    "url": "/static/js/main.fff4aad5.chunk.js"
+  },
+  {
+    "revision": "34a4ed27e280e5c6adb7",
+    "url": "/static/js/1.34a4ed27.chunk.js"
+  },
+  {
+    "revision": "fff4aad525e30a55075c",
+    "url": "/static/css/main.81feb9d9.chunk.css"
+  },
+  {
+    "revision": "d038fbf45ddd50e44b64bedced78506f",
+    "url": "/index.html"
+  }
+];
